78785e0f8f983e239d5f2c8589f6b75d83856e4a
Danilo Ventura <danilo.ventura@scai-consulting.grupposcai.it>
2019-10-22 17:03:04 +0200

    Recipe List implemented

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+    recipes: Recipe[] = [
+      new Recipe('Spaghetti Carbonara',
+      'La vera ricetta degli spaghetti alla crabonara',
+      'https://www.scambiaricette.it/wp-content/uploads/2018/11/carbonara2.jpg.webp'),
+      new Recipe('Arrosto con le patate',
+      'Delizioso arrosto di vitello con le patate al forno',
+      'https://www.donnamoderna.com/wp-content/uploads/2014/09/arrosto-di-vitello-con-patate-dorate-725x545.jpg'),
+      new Recipe('Impepata di cozze',
+      'Le cozze più buone che mai nella tradizione italiana',
+      'https://www.salepepe.it/files/2014/11/cozze-tarantina-200x150.jpg'),
+    ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
