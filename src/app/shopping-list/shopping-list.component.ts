717fad850f20fdc5635456b827033e8daa162ed4
Danilo Ventura <danilo.ventura@scai-consulting.grupposcai.it>
2019-10-23 16:29:49 +0200

    Fine capitolo 3

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from './../models/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('Bacon', 10),
+    new Ingredient('Eggs', 4),
+    new Ingredient('Spaghetti', 400),
+    new Ingredient('Potatoes', 10),
+  ];
+  
   constructor() { }
 
   ngOnInit() {
